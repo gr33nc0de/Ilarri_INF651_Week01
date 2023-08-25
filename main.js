0cb86d5cda1a000e5946e2c2434ef67c4685ac1d
Rafaela Ilarri <r_ilarri@mail.fhsu.edu>
2023-08-25 11:54:46 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+/*
+
+This assignment is both an exercise in JavaScript and in following directions. Follow each step carefully.
+After completing the assigned readings, you should be able to create some basic output with Javascript, and after watching the assigned video, you should be able to use git with your assignment. 
+Go to the following link: https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics
+On that page, scroll down to "A 'hello world' example". Follow the step-by-step instructions to download their code and edit it in your development environment. Also, initialize git to use with your project.
+After completing the instructions from MDN, make sure your page loads as expected. Create a new repository on GitHub named "your-last-name_INF651_Week01". Confirm you have committed all changes to your local repository with git. Then push your local git repository code to your GitHub repository.
+Copy the URL to your GitHub repository.
+Click the title Assignment 01 above to access the submission area for this project. 
+In the submission area, click the "Write Submission" button and paste the URL to your GitHub repository. 
+Do NOT use the "Add Comments" area to submit your assignment. It is only for comments.
+After checking your URL and pasting it in the area that appears after clicking the "Write Submission" button, submit your assignment. 
+
+*/
+
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
